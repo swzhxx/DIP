e8126a20555e97d20f93cfe48574c45e35e5f5e1
swzhxx <549909896@qq.com>
2021-06-29 22:01:28 +0800

    chore(wasm dct): 离散余弦变换

--- dipwasm/src/core/transform/mod.rs
+++ dipwasm/src/core/transform/mod.rs
@@ -1,3 +1,5 @@
+use num_traits::*;
+use std::f64::consts::PI;
 
 use ndarray::prelude::*;
 pub fn dct2d(data: Array2<f64>) -> Array2<f64> {
@@ -7,13 +9,19 @@ pub fn dct2d(data: Array2<f64>) -> Array2<f64> {
     let calc_c = |y: usize, x: usize| -> f64 {
         todo!();
     };
+    let dct = |data: ArrayView<f64, Ix1>, c: f64| -> Array1<f64> {
+        let data = &data
+            * &Array::linspace(0., data.len() as f64, data.len())
+                .map(|&val| return ((PI * c * (2. * val + 1.)) / (2. * v as f64)).cos());
+        data
+    };
 
     // 如果按照公式那么将是O^4的算法。需要优化
     // for y in 0..v {
     //     for x in 0..u {
     //         let c = calc_c(y, x);
     //         let v = data[[y, x]];
-    //         let temp = 
+    //         let temp =
     //     }
     // }
 
