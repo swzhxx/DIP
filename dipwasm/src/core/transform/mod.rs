500af01682ff53fbae0c1474b56eb24a25d046d1
swzhxx <549909896@qq.com>
2021-06-30 22:10:43 +0800

    feat(wasm ): 离散余弦变换

--- dipwasm/src/core/transform/mod.rs
+++ dipwasm/src/core/transform/mod.rs
@@ -1,29 +1,100 @@
+use ndarray::prelude::*;
+use ndarray::Array;
 use num_traits::*;
 use std::f64::consts::PI;
 
-use ndarray::prelude::*;
 pub fn dct2d(data: Array2<f64>) -> Array2<f64> {
     let shape = data.shape();
     let v = shape[0];
     let u = shape[1];
-    let calc_c = |y: usize, x: usize| -> f64 {
-        todo!();
-    };
-    let dct = |data: ArrayView<f64, Ix1>, c: f64| -> Array1<f64> {
-        let data = &data
-            * &Array::linspace(0., data.len() as f64, data.len())
-                .map(|&val| return ((PI * c * (2. * val + 1.)) / (2. * v as f64)).cos());
-        data
+
+    let C = Array::from_shape_fn((u, v), |(y, x)| {
+        if y == 0 && x == 0 {
+            return 1 / v;
+        } else {
+            return 1 / (2 * v);
+        }
+    });
+
+    let dct_matrix = |n: usize| -> Array2<f64> {
+        let M = Array::from_shape_fn((n, n), |(y, x)| {
+            let y = y as f64;
+            let x = x as f64;
+            let n = n as f64;
+            (PI * y * (2. * n + 1.) / (2. * n)).cos() * (PI * y * (2. * n + 1.) / (2. * n)).cos()
+        });
+        M
     };
 
-    // 如果按照公式那么将是O^4的算法。需要优化
-    // for y in 0..v {
-    //     for x in 0..u {
-    //         let c = calc_c(y, x);
-    //         let v = data[[y, x]];
-    //         let temp =
-    //     }
-    // }
+    dct_matrix(v).dot(&data).dot(&dct_matrix(v))
+}
 
-    array![[]]
+fn dct_transform_matrix() -> Array2<f64> {
+    let size = 8.0;
+    Array::from_shape_fn((size as usize, size as usize), |(j, i)| {
+        if j == 0 {
+            1. / size.sqrt()
+        } else {
+            let j = j as f64;
+            let i = i as f64;
+            (2. / size).sqrt() * (PI * (2. * i + 1.) * j / (2. * size)).cos()
+        }
+    })
+}
+
+pub fn dct_2d_by_84_blocks(data: &Array2<f64>) -> Array2<f64> {
+    let size = 8.;
+    let T = dct_transform_matrix();
+
+    let shape = data.shape();
+    //割块
+    let h_blocks = (shape[0] as f64 / size).ceil() as usize;
+    let w_blocks = (shape[1] as f64 / size).ceil() as usize;
+
+    let mut res: Array2<f64> = Array::from_elem((shape[0], shape[1]), 0.);
+
+    for h_count in 0..h_blocks {
+        for w_count in 0..w_blocks {
+            let start_h = h_count * 8;
+            let start_w = w_count * 8;
+            let s = data.slice(s![start_h..(start_h + 8), start_w..(start_w + 8)]);
+            res.slice_mut(s![start_h..(start_h + 8), start_w..(start_w + 8)])
+                .assign(&(&s.dot(&T)));
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ndarray::prelude::*;
+    #[test]
+    fn dct_8_transform_matrix() {
+        let T = dct_transform_matrix();
+        let T1 = array![
+            [0.35355, 0.35355, 0.35355, 0.35355, 0.35355, 0.35355, 0.35355, 0.35355],
+            [0.49039, 0.41573, 0.27779, 0.09755, -0.09755, -0.27779, -0.41573, -0.49039],
+            [0.46194, 0.19134, -0.19134, -0.46194, -0.46194, -0.19134, 0.19134, 0.46194],
+            [0.41573, -0.09755, -0.49039, -0.27779, 0.27779, 0.49039, 0.09755, -0.41573],
+            [0.35355, -0.35355, -0.35355, 0.35355, 0.35355, -0.35355, -0.35355, 0.35355],
+            [0.27779, -0.49039, 0.09755, 0.41573, -0.41573, -0.09755, 0.49039, -0.27779],
+            [0.19134, -0.46194, 0.46194, -0.19134, -0.19134, 0.46194, -0.46194, 0.19134],
+            [0.09755, -0.27779, 0.41573, -0.49039, 0.49039, -0.41573, 0.27779, -0.09755]
+        ];
+        let abs = &T - &T1;
+        assert!(abs.sum() < 0.01);
+    }
+    #[test]
+    fn test_dct_2d_by_84_blocks() {
+        let data = Array2::from_shape_fn((8, 8), |(j, i)| if j == i { 1. } else { 0. });
+        let dct_res = dct_2d_by_84_blocks(&data);
+
+        let dct_transform_matrix = dct_transform_matrix();
+        let dct_res2 = dct_transform_matrix.dot(&data);
+
+        let abs = &dct_res - &dct_res2;
+        assert_eq!(dct_res, dct_res2);
+        // assert!(abs.sum() < 0.01)
+    }
 }
