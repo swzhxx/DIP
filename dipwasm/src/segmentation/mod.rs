a8dc9013bb79bac4f2ca92c259734cb2955c6520
swzhxx <549909896@qq.com>
2021-07-08 23:27:49 +0800

    chore(wasm): graph cuts

--- dipwasm/src/segmentation/mod.rs
+++ dipwasm/src/segmentation/mod.rs
@@ -2,11 +2,13 @@ use js_sys::{Uint32Array, Uint8ClampedArray};
 use ndarray::{prelude::*, OwnedRepr, ViewRepr};
 use num_traits::Pow;
 
-use rs_graph::traits::Indexable;
-use rs_graph::vecgraph::Node;
+use rs_graph::traits::{Directed, Indexable};
+use rs_graph::IndexGraph;
+
 use rs_graph::{
     maxflow::dinic,
-    vecgraph::{Edge, VecGraphBuilder},
+    traits::GraphSize,
+    vecgraph::{Edge, Node, VecGraphBuilder},
     Builder, LinkedListGraph, Net, VecGraph,
 };
 use wasm_bindgen::{prelude::*, Clamped};
@@ -106,28 +108,37 @@ pub fn graph_cuts(
     mut front: Uint32Array,
     mut background: Uint32Array,
 ) -> Result<ImageData, JsValue> {
+    unsafe {
+        web_sys::console::log_1(&format!("start0").into());
+    }
     let get_node_index = |y, x| -> usize { y * width + x };
     let calc_edge_weight = |current: ArrayBase<ViewRepr<&u8>, Dim<[usize; 1]>>,
                             next: ArrayBase<ViewRepr<&u8>, Dim<[usize; 1]>>|
      -> f64 {
         let t: Array1<f64> = (&current - &next).map(|val| (*val as f64).pow(2.));
-        1. / (t.sum() + 1.)
+        let weight = 1. / (t.sum() + 0.00001);
+        // unsafe {
+        //     web_sys::console::log_1(&format!("weight {:?}", weight).into());
+        // }
+        weight
     };
 
-    let data: Array3<u8> = Array::from(data.to_vec())
-        .into_shape((height, width, 4))
-        .unwrap();
-
+    let data: Array3<u8> = Array::from_shape_vec((height, width, 4), data.to_vec()).unwrap();
+    unsafe {
+        web_sys::console::log_1(&format!("start").into());
+    }
     let mut builder_g = VecGraphBuilder::with_capacities(0, 0);
 
     let mut upper: Vec<f64> = vec![];
-
+    let mut nodes: Vec<Node> = vec![];
     for y in 0..height {
         for x in 0..width {
-            builder_g.add_node();
+            let node = builder_g.add_node();
+            nodes.push(node)
         }
     }
     let source = builder_g.add_node();
+
     let sink = builder_g.add_node();
 
     //edge capacity
@@ -139,46 +150,124 @@ pub fn graph_cuts(
                 let right_neighbor = data.slice(s![y, x + 1, ..]);
                 let weight = calc_edge_weight(current, right_neighbor);
                 let nextIndex = get_node_index(y, x + 1);
-                upper.push(weight)
+                upper.push(weight);
+                builder_g.add_edge(nodes[currentIndex], nodes[nextIndex]);
+                upper.push(weight);
+                builder_g.add_edge(nodes[nextIndex], nodes[currentIndex]);
             }
             if y < height - 1 {
                 let bottom_neighbor = data.slice(s![y + 1, x, ..]);
                 let nextIndex = get_node_index(y + 1, x);
                 let weight = calc_edge_weight(current, bottom_neighbor);
-                upper.push(weight)
+                upper.push(weight);
+                builder_g.add_edge(nodes[currentIndex], nodes[nextIndex]);
+                upper.push(weight);
+                builder_g.add_edge(nodes[nextIndex], nodes[currentIndex]);
             }
         }
     }
-
+    unsafe {
+        web_sys::console::log_1(&format!("edge build completed").into());
+    }
     let build_background_front_edge =
-        |points: &mut Uint32Array, node: Node<u32>, upper: &mut Vec<f64>| {
+        |points: &mut Uint32Array,
+         node: Node<u32>,
+         upper: &mut Vec<f64>,
+         builder_g: &mut VecGraphBuilder<u32>| {
             let front_vec = points.to_vec();
             let len = front_vec.len() as usize;
-            let front = Array::from(front_vec)
-                .into_shape(((len / 2) as usize, 2))
-                .unwrap();
-            for i in 0..len {
+            let front = Array::from_shape_vec(((len / 2) as usize, 2), front_vec).unwrap();
+            unsafe {
+                web_sys::console::log_1(&format!("front build completed {:?}", front).into());
+            }
+            for i in 0..front.shape()[0] {
                 let y = front[[i, 0]];
                 let x = front[[i, 1]];
+
                 let currentIndex = get_node_index(y as usize, x as usize);
+                let anode = nodes[currentIndex];
+                // unsafe {
+                //     web_sys::console::log_1(&format!("anode {:?}", anode).into());
+                // }
                 upper.push(MAXIUM as f64);
+                if anode == source {
+                    builder_g.add_edge(anode, node);
+                    upper.push(0.);
+                    builder_g.add_edge(node, sink);
+                } else {
+                    builder_g.add_edge(node, anode);
+                    upper.push(0.);
+                    builder_g.add_edge(source, node);
+                }
+
+                // upper.push(MAXIUM as f64);
+                // builder_g.add_edge(node, anode);
             }
         };
     // 构建前景节点
     {
-        build_background_front_edge(&mut front, source, &mut upper);
+        unsafe {
+            web_sys::console::log_1(&format!("front build start").into());
+        }
+        build_background_front_edge(&mut front, source, &mut upper, &mut builder_g);
     }
     // 构建背景
     {
-        build_background_front_edge(&mut background, sink, &mut upper);
+        unsafe {
+            web_sys::console::log_1(&format!("background build start").into());
+        }
+        build_background_front_edge(&mut background, sink, &mut upper, &mut builder_g);
     }
     unsafe {
         web_sys::console::log_1(&format!("graph build completed").into());
     }
     let g = builder_g.into_graph();
 
-    // maxflow mincut 
-    let (value, flow, mut mincut) = dinic(&g, source, sink, |e| (upper[e.index()] * 10000.) as u32);
-    // g.add_edge(source, sink);
-    todo!()
+    // maxflow mincut
+    let (value, flow, mut mincut) = dinic(&g, source, sink, |e| (upper[e.index()] * 1000.) as u32);
+
+    let mut fronts = vec![];
+    // struct Dfs<'s, 'r, 'a> {
+    //     f: &'s dyn Fn(&'r Dfs, &'a VecGraph<u32>, Node, &'a mut Vec<(usize, usize)>),
+    // }
+
+    // let dfs = Dfs {
+    //     f: &|dfs: &Dfs, g: &VecGraph<u32>, startNode: Node, c: &mut Vec<(usize, usize)>| {
+    //         g.outedges(source).for_each(|(e, n)| {
+    //             {
+    //                 c.push((g.node_id(startNode), g.node_id(n)));
+    //             }
+    //             (dfs.f)(dfs, g, n, c)
+    //         });
+    //     },
+    // };
+
+    // (dfs.f)(&dfs, &g, source, &mut fronts);
+
+    // let dfs = |g: &VecGraph<u32>, startNode: Node, c: &mut Vec<(usize, usize)>| {
+    //     g.outedges(source).for_each(|(e, n)| {
+    //         {
+    //             fronts.push((g.node_id(startNode), g.node_id(n)));
+    //         }
+    //         dfs(&g, n, fronts)
+    //     });
+    // };
+    // dfs(&g, source, &mut fronts);
+    // g.outedges(source)
+    //     .for_each(|(e, n)| dfs(&g, n, &mut fronts));
+
+    // let mut builder = VecGraphBuilder::with_capacities(0, 0);
+    // g.nodes().for_each(|n| {
+    //     // let nid = g.node2id(n);
+    //     builder.add_node();
+    // });
+    // g.edges()
+    //     .filter(|e| (upper[e.index()]).abs() > 0.0001)
+    //     .for_each(|e| builder.add_edge(e));
+
+    ImageData::new_with_u8_clamped_array_and_sh(
+        Clamped(&vec![0 as u8, 0 as u8, 0 as u8, 0 as u8]),
+        1,
+        1,
+    )
 }
--- dipwasm/www/src/views/segmentation/index.tsx
+++ dipwasm/www/src/views/segmentation/index.tsx
@@ -1,6 +1,7 @@
 import React, { MouseEventHandler, useRef, useState, MouseEvent } from 'react'
 import { Button } from 'antd'
 import _ from 'lodash'
+import { Splat } from '@/wasm'
 const FRONT = 1
 const BACKGROUND = 2
 const NONE = 3
@@ -13,6 +14,7 @@ export default (): JSX.Element => {
     isDown: false,
     fronts: new Map(),
     backgrounds: new Map(),
+    imageData: undefined,
   })
 
   const onLoad = () => {
@@ -21,28 +23,49 @@ export default (): JSX.Element => {
       canvasRef.current.getContext('2d')
     if (ctx == null) return
     ctx.drawImage(imgRef?.current as HTMLImageElement, 0, 0)
+    setState({ ...state, imageData: ctx.getImageData(0, 0, 512, 512).data })
     // let rect: DOMRect = canvasRef?.current?.getBoundingClientRect()
   }
 
   const onMouseDown = (e: MouseEvent) => {
     if (state.status == NONE) return
     setState({ ...state, isDown: true })
+    const ctx = canvasRef?.current?.getContext('2d')
+    ctx?.beginPath()
+    ctx?.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
   }
   const onMouseMove = (e: MouseEvent) => {
     if (state.status == NONE || state.isDown == false) return
     console.log(e)
     const nativeEvent = e.nativeEvent
     const { offsetX, offsetY } = nativeEvent
+    let _ctx = canvasRef?.current?.getContext('2d')
+    if (!(_ctx instanceof CanvasRenderingContext2D)) {
+      throw new Error('Failed to get 2D context')
+    }
+    const ctx: CanvasRenderingContext2D = _ctx
+    if (ctx == null) return
     if (state.status === BACKGROUND) {
       state.backgrounds.set(`${offsetX},${offsetY}`, 1)
+      ctx.strokeStyle = `rgb(255,0,0)`
     }
     if (state.status === FRONT) {
-      state.front.set(`${offsetX},${offsetY}`, 1)
+      state.fronts.set(`${offsetX},${offsetY}`, 1)
+      ctx.strokeStyle = `rgb(0,255,0)`
     }
+
+    ctx?.lineTo(offsetX, offsetY)
+    ctx?.stroke()
+    // ctx?.closePath()
+    // ctx?.beginPath()
+    // ctx?.lineTo(offsetX, offsetY)
+    // ctx?.moveTo(offsetX, offsetY)
   }
   const onMouseUp = (e: MouseEvent) => {
     setState({ ...state, isDown: false })
     console.log(e)
+    let ctx = canvasRef?.current?.getContext('2d')
+    ctx?.closePath()
   }
 
   const map2Unit32Array = (hashmap: Map<string, string>) => {
@@ -52,6 +75,13 @@ export default (): JSX.Element => {
     })
     let ps: number[] = _.flatten(points)
     return Uint32Array.from(ps)
+  }
+
+  const handleGraphCuts = () => {
+    const imageData = state.imageData
+    const frontPixels = map2Unit32Array(state.fronts)
+    const backgroundPixels = map2Unit32Array(state.backgrounds)
+    Splat.graphCuts(imageData, 512, 512, frontPixels, backgroundPixels)
   }
   return (
     <div>
@@ -75,13 +105,15 @@ export default (): JSX.Element => {
           onMouseUp(e)
         }}
       ></canvas>
-      <Button onClick={() => setState({ ...state, status: FRONT })}>
-        前景
-      </Button>
-      <Button onClick={() => setState({ ...state, status: BACKGROUND })}>
-        背景
-      </Button>
-      <Button>分割</Button>
+      <div>
+        <Button onClick={() => setState({ ...state, status: FRONT })}>
+          前景
+        </Button>
+        <Button onClick={() => setState({ ...state, status: BACKGROUND })}>
+          背景
+        </Button>
+        <Button onClick={handleGraphCuts}>分割</Button>
+      </div>
     </div>
   )
 }
