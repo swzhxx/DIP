9f88e65a0439abd9d640cbeb401354f9d693fd29
zhx <549909896@qq.com>
2021-08-21 08:41:58 +0800

    feat(slam ): 相机位姿估计

---
+++ slam/ch06/pose_esimation_2d2d.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <opencv2/core/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+using namespace std ;
+using namespace cv ; 
+
+void find_feature_matches(
+  const Mat &img_1, const Mat &img_2,
+  std::vector<KeyPoint> &keypoints_1,
+  std::vector<KeyPoint> &keypoints_2,
+  std::vector<DMatch> &matches);
+
+void pose_estimation_2d2d(
+  std::vector<KeyPoint> keypoints_1,
+  std::vector<KeyPoint> keypoints_2,
+  std::vector<DMatch> matches,
+  Mat &R, Mat &t);
+
+// 像素坐标转相机归一化坐标
+Point2d pixel2cam(const Point2d &p, const Mat &K);
+
+int main(int argc , char ** argc){
+  if (argc !=3){
+    cout << "usage: pose_estimation_2d2d img1 img2" << endl;
+    return 1
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
+  Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+  //-- 第一步:检测 Oriented FAST 角点位置
+  detector->detect(img_1, keypoints_1);
+  detector->detect(img_2, keypoints_2);
+
+  //-- 第二步:根据角点位置计算 BRIEF 描述子
+  descriptor->compute(img_1, keypoints_1, descriptors_1);
+  descriptor->compute(img_2, keypoints_2, descriptors_2);
+
+  //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
+  vector<DMatch> match;
+  //BFMatcher matcher ( NORM_HAMMING );
+  matcher->match(descriptors_1, descriptors_2, match);
+
+  //-- 第四步:匹配点对筛选
+  double min_dist = 10000, max_dist = 0;
+
+  //找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
+  for (int i = 0; i < descriptors_1.rows; i++) {
+    double dist = match[i].distance;
+    if (dist < min_dist) min_dist = dist;
+    if (dist > max_dist) max_dist = dist;
+  }
+
+  printf("-- Max dist : %f \n", max_dist);
+  printf("-- Min dist : %f \n", min_dist);
+
+  //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
+  for (int i = 0; i < descriptors_1.rows; i++) {
+    if (match[i].distance <= max(2 * min_dist, 30.0)) {
+      matches.push_back(match[i]);
+    }
+  }
+}
+
+Point2d pixel2cam(const Point2d &p, const Mat &K) {
+  return Point2d
+    (
+      (p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
+      (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1)
+    );
+}
