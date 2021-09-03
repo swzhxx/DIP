ce96a12bc6cc77b1b5f0af1a740327a40e099258
swzhxx <549909896@qq.com>
2021-09-03 10:09:24 +0800

    fix(slam-rust ): lm 算法调试 bugfix

--- slam-rust/slam-core/src/optimize/mod.rs
+++ slam-rust/slam-core/src/optimize/mod.rs
@@ -5,23 +5,41 @@ use ndarray_linalg::Solve;
 use ndarray_stats::QuantileExt;
 use num_traits::Num;
 
+pub type ErrorClousure<T> = Rc<Box<dyn Fn(&ArrayView1<f64>, usize, &T, &T) -> f64>>;
+pub type JacobianClousure<T> = Rc<Box<dyn Fn(&ArrayView1<f64>, usize, &T) -> Array1<f64>>>;
 /// Levenberg Marquardt Algorithm
-pub struct LM {
+pub struct LM<'a, T> {
     damp: Option<f64>,
-    datas: usize,
-    error_closure: Rc<Box<dyn Fn(&ArrayView1<f64>, usize) -> f64>>,
-    jacobian_closure: Rc<Box<dyn Fn(&ArrayView1<f64>, usize) -> Array1<f64>>>,
+    inputs: &'a Vec<T>,
+    outputs: &'a Vec<T>,
+    error_closure: ErrorClousure<T>,
+    jacobian_closure: JacobianClousure<T>,
 }
 
-impl LM {
+impl<'a, T> LM<'a, T> {
+    fn new(
+        inputs: &'a Vec<T>,
+        outputs: &'a Vec<T>,
+        error_closure: ErrorClousure<T>,
+        jacobian_closure: JacobianClousure<T>,
+    ) -> Self {
+        LM {
+            damp: None,
+            inputs,
+            outputs,
+            error_closure,
+            jacobian_closure,
+        }
+    }
+
     /// 初始化阻尼系数
     fn init_damp(&mut self, j: &Array2<f64>) {
         let h = j.dot(&j.t());
         let max = h.diag().max().unwrap().clone();
-        self.damp = Some(max);
+        self.damp = Some(max * 0.001);
     }
 
-    pub fn slove(&mut self, args: &Array1<f64>, stop_delta: f64) -> Array1<f64> {
+    pub fn optimize(&mut self, args: &Array1<f64>, upsilon: f64) -> Array1<f64> {
         let shape = args.shape();
         let mut fitting = args.to_shape((shape[0], 1)).unwrap().to_owned();
 
@@ -39,7 +57,7 @@ impl LM {
             fitting = self.update(&jaco, &errors, &fitting);
             match last_cost {
                 Some(lc) => {
-                    if (lc - cost).abs() < stop_delta.abs() {
+                    if (lc - cost).abs() < upsilon.abs() {
                         break;
                     }
                     last_cost = Some(cost);
@@ -83,8 +101,8 @@ impl LM {
     fn error(&self, args: &ArrayView1<f64>) -> Array2<f64> {
         let mut errors = vec![];
         let error_closure = self.error_closure.clone();
-        for row_i in 0..self.datas {
-            let error = error_closure(args, row_i);
+        for row_i in 0..self.inputs.len() {
+            let error = error_closure(args, row_i, &self.inputs[row_i], &self.outputs[row_i]);
             errors.push(error);
         }
         Array2::from_shape_vec((errors.len(), 1), errors).unwrap()
@@ -93,10 +111,59 @@ impl LM {
     fn jaco(&self, args: &ArrayView1<f64>) -> Array2<f64> {
         let mut jaco = Array2::<f64>::zeros((0, args.len()));
         let jacobian_closure = self.jacobian_closure.clone();
-        for row_i in 0..self.datas {
-            let j = jacobian_closure(args, row_i);
+        for row_i in 0..self.inputs.len() {
+            let j = jacobian_closure(args, row_i, &self.inputs[row_i]);
             jaco.push(Axis(0), j.slice(s![..]));
         }
         jaco
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ndarray::{Array1, ArrayView0};
+    #[test]
+    fn test_lm_algoritm() {
+        let real = Array1::from_shape_vec(3, vec![1.0, 2.0, 1.0]).unwrap();
+        let mut fitting = Array1::from_shape_vec(3, vec![2.0, -1.0, 5.0]).unwrap();
+        let inputs = {
+            let mut v = vec![];
+            for k in 0..100 {
+                v.push(k as f64 / 100.);
+            }
+            v
+        };
+
+        let outputs: Vec<f64> = inputs
+            .iter()
+            .map(|input| return (input * real[0] * real[0] + input * real[1] + real[2]).exp())
+            .collect();
+
+        let mut lm: LM<f64> = LM::new(
+            &inputs,
+            &outputs,
+            Rc::new(Box::new(
+                |args: &ArrayView1<f64>, row_i: usize, input, output| -> f64 {
+                    let input = input;
+                    let error =
+                        output - (input * args[0] * args[0] + input * args[1] + args[2]).exp();
+                    error
+                },
+            )),
+            Rc::new(Box::new(
+                |args: &ArrayView1<f64>, row_i: usize, input| -> Array1<f64> {
+                    let a = -input
+                        * input
+                        * (args[0] * args[0] * input + args[1] * input + args[2]).exp();
+                    let b = -input * (args[0] * args[0] * input + args[1] * input + args[2]).exp();
+                    let c = -(args[0] * args[0] * input + args[1] * input + args[2]).exp();
+                    Array1::from_vec(vec![a, b, c])
+                },
+            )),
+        );
+
+        let fitting = lm.optimize(&fitting, 0.0001);
+        eprintln!("{:?}", fitting)
     }
 }
